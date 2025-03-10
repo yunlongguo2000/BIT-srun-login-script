97a5e8035155d5d4258720534c315df71be1d676
Yunlong Guo <yunlong.guo2000@outlook.com>
2025-03-10 10:31:41 +0800

    Revert "update for windows"
    This reverts commit dbdbef8cf0f3f66f30b2b794829ee1fe89bd9e09.

--- AutoLoad.py
+++ AutoLoad.py
@@ -2,25 +2,16 @@
 # Author: Sunhr
 # Keep BIT-Web online
 
-import os
 import time
 import random
-import getpass
 import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import FirefoxOptions, ChromeOptions
+from selenium.webdriver import FirefoxOptions,ChromeOptions
 
-# 在程序启动时保存当前进程的 PID
-def write_pid_file():
-    pid_file_path = "F:\\BIT-srun-login-script\\autoload.pid"
-    with open(pid_file_path, "w") as f:
-        f.write(str(os.getpid()))
-        print("PID number is:", os.getpid())
-        print("PID file created at", pid_file_path)
 
-def loopLoad(usrname, passwd, browserChoice='firefox'):
-    while True:
+def loopLoad(usrname,passwd,browserChoice='firefox'):
+    while(1):
         if browserChoice == 'firefox':
             opts = FirefoxOptions()
             opts.add_argument("--headless")
@@ -31,11 +22,11 @@ def loopLoad(usrname, passwd, browserChoice='firefox'):
             browser = webdriver.Chrome(options=opts)
         time.sleep(1)
         
-        el = lambda id: browser.find_element(By.ID, id)
+        el = lambda id : browser.find_element(By.ID, id)
         try:
             browser.get('http://10.0.0.55/')
             time.sleep(1)
-            try:
+            try: 
                 if el("logout"):
                     print("Bit-Web still OK!")
                     browser.close()
@@ -50,23 +41,22 @@ def loopLoad(usrname, passwd, browserChoice='firefox'):
                 time.sleep(2)
                 print("Bit-Web OK!")
             
-        except Exception as e:
+        except:
             browser.close()
-            print("Bit-Web Failed! Error:", e)
+            print("Bit-Web Failed!")
             continue
 
 if __name__ == '__main__':
-    write_pid_file()
     browerDict = {'1':'firefox','2':'chrome'}
     browerIdx = input('Please choose your brower number ( 1 for Firefox; 2 for Chrome ):')
     usrname = input("Please input username: ")
-    passwd = getpass.getpass("Please input passwd: ") 
-    
-    
-    # 在顶层使用无限循环捕获所有异常，避免脚本异常退出
-    while True:
-        try:
-            loopLoad(usrname, passwd, browerDict[browerIdx])
-        except Exception as err:
-            print("Unhandled exception:", err)
-            time.sleep(10)
+    passwd = input("Please input passwd: ")
+
+    loopLoad(usrname,passwd,browerDict[browerIdx])
+
+    # usrname = args.usrname
+    # passwd = args.passwd
+    # print(usrname,passwd)
+
+
+
